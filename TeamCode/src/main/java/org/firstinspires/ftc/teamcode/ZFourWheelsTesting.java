06c49c37aa680009720be75825e96ef9dddd59f9
admiralwaffle4 <56054320+admiralwaffle4@users.noreply.github.com>
2021-12-09 15:49:46 -0500

    trying this again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZFourWheelsTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZFourWheelsTesting.java
@@ -45,9 +45,9 @@ public class ZFourWheelsTesting extends LinearOpMode {
         }
 
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,  48,  48,48, 48);  // S1: Forward 48 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   -12, -12, 12, 12);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, 24, 24, 24, 24);  // S3: Reverse 24 Inches with 4 Sec timeout
 
 
 
@@ -57,27 +57,23 @@ public class ZFourWheelsTesting extends LinearOpMode {
 
 
     }
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
+    public void encoderDrive(double speed, double frontLeft, double backLeft, double frontRight, double backRight) {
+        double[] targets = new double[4];
 
         // Ensure that the opmode is still active
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newLeftTarget = wheels[2].getCurrentPosition() + (int) (leftInches * COUNTS_PER_CM);
-            newRightTarget = wheels[0].getCurrentPosition() + (int) (rightInches * COUNTS_PER_CM);
-            wheels[0].setTargetPosition(newRightTarget);
-            wheels[1].setTargetPosition(newRightTarget);
-            wheels[2].setTargetPosition(newLeftTarget);
-            wheels[3].setTargetPosition(newLeftTarget);
+            targets[0] = wheels[0].getCurrentPosition() + (int) (frontRight * COUNTS_PER_CM);
+            targets[1] = wheels[1].getCurrentPosition() + (int) (backRight * COUNTS_PER_CM);
+            targets[2] = wheels[2].getCurrentPosition() + (int) (frontLeft * COUNTS_PER_CM);
+            targets[3] = wheels[3].getCurrentPosition() + (int) (backLeft * COUNTS_PER_CM);
 
             runtime.reset();
-            for (DcMotor wheel : wheels) {
-                wheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                wheel.setPower((Math.abs(speed)));
+            for (int i = 0; i < 4; i++) {
+                wheels[i].setTargetPosition((int) targets[i]);
+                wheels[i].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                wheels[i].setPower(Math.abs(speed));
             }
 
             // keep looping while we are still active, and there is time left, and both motors are running.
@@ -86,12 +82,10 @@ public class ZFourWheelsTesting extends LinearOpMode {
             // always end the motion as soon as possible.
             // However, if you require that BOTH motors have finished their moves before the robot continues
             // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (wheels[2].isBusy() && wheels[0].isBusy())) {
+            while (opModeIsActive() && (wheels[0].isBusy() && wheels[1].isBusy() && wheels[2].isBusy() && wheels[3].isBusy())) {
 
                 // Display it for the driver.
-                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path1", "Running to %7d :%7d :%7d, :%7d", targets[0], targets[1], targets[2], targets[3]);
                 for (DcMotor wheel : wheels) {
                     telemetry.addData("Path2", "Starting at %7d", wheel.getCurrentPosition());
                     telemetry.update();
